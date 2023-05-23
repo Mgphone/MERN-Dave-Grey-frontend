58066d92d335dbcc8b397107c6110e79e5823e34
Mgphone <51868037+Mgphone@users.noreply.github.com>
2023-05-23 15:34:05 +0100

    chapter 5 react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
+import {Routes,Route} from 'react-router-dom'
+import Layout from './components/Layout';
+import Public from './components/Public';
+import DashLayout from './components/DashLayout';
+import Login from './Features/auth/Login';
+import Welcome from './Features/auth/Welcome';
+import NoteList from './Features/notes/NoteList';
+import UserList from './Features/users/UserList';
 
 function App() {
   return (
-    <h1>Welcome</h1>
+    <Routes>
+      <Route path='/' element={<Layout />} />
+      <Route index element={<Public />} />
+      <Route path='login' element={<Login />} />
+
+      <Route path='dash' element={<DashLayout />} >
+        <Route index element={<Welcome />} />
+        <Route path='notes'>
+          <Route index element={<NoteList />} />
+        </Route>{/*end note*/}
+        <Route path='users'>
+          <Route index element={<UserList />} />
+        </Route>{/* end user*/ }
+      </Route>{/* enddash */}      
+    </Routes>
   );
 }
 
